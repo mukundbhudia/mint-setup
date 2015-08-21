282717014ee15efa5dbf3ad4da035e0d824cf72e
Mukund Bhudia <mukund.bhudia@clientrelationship.com>
2015-08-21 17:09:27 +0100

    Added gufw, gitg, gparted and mysql workbench + CLI tools

--- setup.sh
+++ setup.sh
@@ -5,3 +5,13 @@ sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 sudo apt-get update
 sudo apt-get install atom
 sudo apt-get install grub-customizer
+sudo apt-get install gufw
+sudo apt-get install gitg
+sudo apt-get install gparted
+
+#Download + install mysql workbench
+wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
+sudo apt-get install mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
+
+#Install mysql cli
+sudo apt-get install mysql-client-core-5.6
