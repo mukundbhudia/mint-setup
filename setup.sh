f68ed4325beb6e9f9a8af1c8ea255b4529212cba
Mukund Bhudia <mukund.bhudia@clientrelationship.com>
2015-08-21 16:52:13 +0100

    Added Grub customiser

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # Linux mint setup
-# Install Atom
+# Install PPAs
 sudo add-apt-repository ppa:webupd8team/atom
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 sudo apt-get update
 sudo apt-get install atom
+sudo apt-get install grub-customizer
