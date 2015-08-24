b03dff0408ccd51eb1141df0661b9857de001835
Mukund Bhudia <mukund.bhudia@clientrelationship.com>
2015-08-24 16:53:35 +0100

    Removed sudo from commands

--- setup.sh
+++ setup.sh
@@ -12,19 +12,19 @@ else
     echo "Home setup installing..."
     fi
     # Install PPAs
-    sudo add-apt-repository ppa:webupd8team/atom
-    sudo add-apt-repository ppa:danielrichter2007/grub-customizer
-    sudo apt-get update
-    sudo apt-get install atom
-    sudo apt-get install grub-customizer
-    sudo apt-get install gufw
-    sudo apt-get install gitg
-    sudo apt-get install gparted
+    add-apt-repository ppa:webupd8team/atom
+    add-apt-repository ppa:danielrichter2007/grub-customizer
+    apt-get update
+    apt-get install atom
+    apt-get install grub-customizer
+    apt-get install gufw
+    apt-get install gitg
+    apt-get install gparted
 
     #Download + install mysql workbench
     wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
-    sudo apt-get install mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
+    apt-get install mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
 
     #Install mysql cli
-    sudo apt-get install mysql-client-core-5.6
+    apt-get install mysql-client-core-5.6
 fi
