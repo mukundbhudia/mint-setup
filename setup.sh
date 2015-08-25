ef71256aab6b4fd16c2d0b8a91de477cf952759e
Mukund Bhudia <mukund.bhudia@clientrelationship.com>
2015-08-25 14:22:43 +0100

    Added check to see if a user is root before executing the script

--- setup.sh
+++ setup.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 # Linux mint setup
-echo -n "Home or work setup > "
-read setupType
-echo "You entered: $setupType"
-# TODO: Fix '-ne' usage. Need a string comparison operator
-if [ $setupType -ne "work" ] || [ $setupType -ne "home" ]; then
-    echo "You must enter either home or work."
+
+# Check if the user can run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
 else
-    if [ $setupType -eq "work" ]; then
-    echo "Work setup installing..."
-elif [ $setupType -eq "home" ]; then
-    echo "Home setup installing..."
+
+    echo -n "Home or work setup > "
+    read setupType
+    echo "You entered: $setupType"
+    # TODO: Fix '-ne' usage. Need a string comparison operator
+
+    if [ $setupType -ne "work" ] || [ $setupType -ne "home" ]; then
+        echo "You must enter either home or work."
+    else
+        if [ $setupType -eq "work" ]; then
+            echo "Work setup installing..."
+        elif [ $setupType -eq "home" ]; then
+            echo "Home setup installing..."
+        fi
+        # Generic setup
+        echo "Installing general software..."
+        add-apt-repository ppa:webupd8team/atom
+        add-apt-repository ppa:danielrichter2007/grub-customizer
+        apt-get update
+        apt-get install atom
+        apt-get install grub-customizer
+        apt-get install gufw
+        apt-get install gitg
+        apt-get install gparted
+
+        #Download + install mysql workbench
+        wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
+        apt-get install mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
+
+        #Install mysql cli
+        apt-get install mysql-client-core-5.6
     fi
-    # Install PPAs
-    add-apt-repository ppa:webupd8team/atom
-    add-apt-repository ppa:danielrichter2007/grub-customizer
-    apt-get update
-    apt-get install atom
-    apt-get install grub-customizer
-    apt-get install gufw
-    apt-get install gitg
-    apt-get install gparted
-
-    #Download + install mysql workbench
-    wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
-    apt-get install mysql-workbench-community-6.3.3-1ubu1404-amd64.deb
-
-    #Install mysql cli
-    apt-get install mysql-client-core-5.6
 fi
